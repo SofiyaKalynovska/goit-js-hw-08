f959779f2946e7f07ef91ba9e91658a610667e77
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-08-08 02:42:34 +0300

    add destructure

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-function onTimeUpdate(data) {
-  localStorage.setItem("videoplayer-current-time", data.seconds)
+function onTimeUpdate({seconds}) {
+  localStorage.setItem("videoplayer-current-time", seconds)
 };
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time" || 0))
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")) || 0
 );
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
